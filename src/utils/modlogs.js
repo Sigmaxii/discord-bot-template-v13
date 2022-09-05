a2cef7294325bc550b4adaa68f8519113c9d44ac
Sigma <67722289+Sigmaxii@users.noreply.github.com>
2022-09-05 18:54:20 +0300

    Update modlogs.js

--- src/utils/modlogs.js
+++ src/utils/modlogs.js
@@ -7,12 +7,12 @@ const db = new QuickDB();
 
 exports.modLog = async function(guild, text) {
 	if (guild && text) {
-		let channel = await db.get(`modlog_${guild.id}`);
+		let channel = await db.get(`modlog_${guild.id}`); // dont change this
     var sChannel = guild.channels.cache.get(channel)
             if (!sChannel) return;
 
 		if (channel) {
-			var embed = new MessageEmbed();
+			var embed = new MessageEmbed(); // this is the modlog embed, test it via the ban command and if change it if you'd like , but make sure that ${text} will be there, otherwise there will be no point of having modlogs
       embed.setTitle("Moderation Logs");
 			embed.setDescription(`${text} \n <t:${(Date.now() / 1000) | 0}:R>`);
 			embed.setColor('#00ffff');
