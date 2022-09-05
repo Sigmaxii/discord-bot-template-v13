11f003219375bb2df12c3d60f30affb07f6a9db1
Sigma <67722289+Sigmaxii@users.noreply.github.com>
2022-09-05 17:05:43 +0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,30 +1,16 @@
 module.exports = {
-   // token:"OTI5MjIxNDYwNjEzMDA1MzYz.G3RSg3.FrzgYPgCSK1eabIbLLjaLCSFQ53eDHq40G2bo4"
-  token: "NzM3MDQwNTE2Njg3OTg2ODA4.Xx3keA.tvYFtlMIFg5-LCKESoy4KDGyfzA",  
+  token: "BOT-TOKEN",  
     prefix: ",",
-  id: "737040516687986808",
-    ownerID: "453370780387115019", 
-    SpotifyID: "8d4acb2d18db4c1bb4e1936cba43b238",
-    SpotifySecret: "80a004de48534bb6bc82ee600f7f5054",
-    mongourl: "mongodb://sigmaxii:albanyolo@sigma-shard-00-00.7ua83.mongodb.net:27017,sigma-shard-00-01.7ua83.mongodb.net:27017,sigma-shard-00-02.7ua83.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-qdwbnf-shard-0&authSource=admin&retryWrites=true&w=majority", 
+  id: "BOT-ID",
+    ownerID: "OWNER-ID", 
+    mongourl: "MONGO-URI", // If you dont have a mongo uri then your bot wont be able to have changeable prefix  
     embedColor: "#00ffff", 
-    avatar: "asda",
-    logs: "937090534957256714",
-  invite: "https://discord.com/api/oauth2/authorize?client_id=737040516687986808&permissions=1378046241911&scope=bot%20applications.commands",
+    avatar: "AVATAR-URL", //or file 
+    logs: "CHANNEL-ID", 
+  invite: "BOT-INVITE-URL",
   suggestions: "948876964595380255",
   bugLogs: "948876983520071710",
-  ready: "994055143702155375",
-  status: "GLOBAL",
-
-  nodes: {
-     
-     host: "34.68.224.132",
-      port: 443,
-      password: "youshallnotpass",
-      id: "Sigma's Music Lavalink",
-      retryDelay: 3000,
-      secure: false
-    
-    },
- 
+  ready: "CHANNEL-ID", //bot on ready message
+  status: "TESTING", //SWITCH IT TO GLOBAL IF YOUR SLASH COMMANDS ARE READY
+   guildId: 'GUILD-ID', // add a guild id that u want to test your slash commands , if status is global then guild id is not needed
 }
